b4f5accd31ae2ddd48e432eb4709d7ce7b9f9550
Christopher Harrison <chrhar@microsoft.com>
2017-01-24 11:12:39 -0800

    Adding dialogAction sample

--- Node/core-dialogAction/bot.js
+++ Node/core-dialogAction/bot.js
@@ -8,6 +8,32 @@ const connector = new builder.ChatConnector({
 
 const bot = module.exports = new builder.UniversalBot(connector);
 
+// register the dialogAction
+// each dialogAction needs a name,
+// the name of the dialog to call, 
+// and what regex it should look for
+bot.beginDialogAction('settings', '/settings', { matches: /^settings/ });
+
+bot.dialog('/settings', [
+    // the dialog is just a normal bot dialog
+    // you can perform whatever operations you need to perform
+    (session, args, next) => {
+        builder.Prompts.text(session, `After receiving your message, control will return to where you left off.`);
+    },
+    (session, results, next) => {
+        // this will return to the step in the prior dialog
+        session.endDialog('... returning to the prior step.');
+    },
+]);
+
+bot.beginDialogAction('cancel', '/cancel', { matches: /^cancel/ });
+bot.dialog('/cancel', [
+    (session, args, next) => {
+        // this will end the entire conversation
+        session.endConversation('Conversation ended. Send a message to start a new one.');
+    },
+]);
+
 // In a bot, a conversation can hold a collection of dialogs.
 
 // Each dialog is designed to be a self-contained unit that can
@@ -16,122 +42,32 @@ const bot = module.exports = new builder.UniversalBot(connector);
 
 bot.dialog('/', [
     (session, args, next) => {
-        session.send(`Hi there! I'm a sample bot showing how multiple dialogs work.`);
+        session.send(`Hi there! I'm a sample bot showing how you can create global commands.`);
+        session.send('Typing `cancel` will restart the conversation, while `settings` allows you to test starting a side dialog that picks up where you left off.');
         session.send(`Let's start the first dialog, which will ask you your name.`);
 
-        // Launch the getName dialog using beginDialog
-        // When beginDialog completes, control will be passed
-        // to the next function in the waterfall
         session.beginDialog('/getName');
     },
     (session, results, next) => {
-        // executed when getName dialog completes
-        // results parameter contains the object passed into endDialogWithResults
-
-        // check for a response
         if (results.response) {
             const name = session.privateConversationData.name = results.response;
-
-            // When calling another dialog, you can pass arguments in the second parameter
             session.beginDialog('/getAge', { name: name });
         } else {
-            // no valid response received - End the conversation
             session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
         }
     },
     (session, results, next) => {
-        // executed when getAge dialog completes
-        // results parameter contains the object passed into endDialogWithResults
-
-        // check for a response
         if (results.response) {
             const age = session.privateConversationData.age = results.response;
             const name = session.privateConversationData.name;
 
             session.endConversation(`Hello ${name}. You are ${age}`);
         } else {
-            // no valid response received - End the conversation
             session.endConversation(`Sorry, I didn't understand the response. Let's start over.`);
         }
     },
 ]);
 
-bot.dialog('/getName', [
-    (session, args, next) => {
-        // store reprompt flag
-        if(args) {
-            session.dialogData.isReprompt = args.isReprompt;
-        }
+bot.dialog('/getName', require('./getNameDialog.js'));
 
-        // prompt user
-        builder.Prompts.text(session, 'What is your name?');
-    },
-    (session, results, next) => {
-        const name = results.response;
-
-        if (!name || name.trim().length < 3) {
-            // Bad response. Logic for single re-prompt
-            if (session.dialogData.isReprompt) {
-                // Re-prompt ocurred
-                // Send back empty string
-                session.endDialogWithResult({ response: '' });
-            } else {
-                // Set the flag
-                session.send('Sorry, name must be at least 3 characters.');
-
-                // Call replaceDialog to start the dialog over
-                // This will replace the active dialog on the stack
-                // Send a flag to ensure we only reprompt once
-                session.replaceDialog('/getName', { isReprompt: true });
-            }
-        } else {
-            // Valid name received
-            // Return control to calling dialog
-            // Pass the name in the response property of results
-            session.endDialogWithResult({ response: name.trim() });
-        }
-    }
-]);
-
-bot.dialog('/getAge', [
-    (session, args, next) => {
-        let name = session.dialogData.name = 'User';
-
-        if (args) {
-            // store reprompt flag
-            session.dialogData.isReprompt = args.isReprompt;
-
-            // retrieve name
-            name = session.dialogData.name = args.name;
-        }
-
-        // prompt user
-        builder.Prompts.number(session, `How old are you, ${name}?`);
-    },
-    (session, results, next) => {
-        const age = results.response;
-
-        // Basic validation - did we get a response?
-        if (!age || age < 13 || age > 90) {
-            // Bad response. Logic for single re-prompt
-            if (session.dialogData.isReprompt) {
-                // Re-prompt ocurred
-                // Send back empty string
-                session.endDialogWithResult({ response: '' });
-            } else {
-                // Set the flag
-                session.dialogData.didReprompt = true;
-                session.send(`Sorry, that doesn't look right.`);
-                // Call replaceDialog to start the dialog over
-                // This will replace the active dialog on the stack
-                session.replaceDialog('/getAge', 
-                    { name: session.dialogData.name, isReprompt: true });
-            }
-        } else {
-            // Valid city received
-            // Return control to calling dialog
-            // Pass the city in the response property of results
-            session.endDialogWithResult({ response: age });
-        }
-    }
-]);
+bot.dialog('/getAge', require('./getAgeDialog.js'));
---
+++ Node/core-dialogAction/getAgeDialog.js
@@ -0,0 +1,44 @@
+const builder = require('botbuilder');
+
+module.exports = [
+    (session, args, next) => {
+        let name = session.dialogData.name = 'User';
+
+        if (args) {
+            // store reprompt flag
+            session.dialogData.isReprompt = args.isReprompt;
+
+            // retrieve name
+            name = session.dialogData.name = args.name;
+        }
+
+        // prompt user
+        builder.Prompts.number(session, `How old are you, ${name}?`);
+    },
+    (session, results, next) => {
+        const age = results.response;
+
+        // Basic validation - did we get a response?
+        if (!age || age < 13 || age > 90) {
+            // Bad response. Logic for single re-prompt
+            if (session.dialogData.isReprompt) {
+                // Re-prompt ocurred
+                // Send back empty string
+                session.endDialogWithResult({ response: '' });
+            } else {
+                // Set the flag
+                session.dialogData.didReprompt = true;
+                session.send(`Sorry, that doesn't look right.`);
+                // Call replaceDialog to start the dialog over
+                // This will replace the active dialog on the stack
+                session.replaceDialog('/getAge', 
+                    { name: session.dialogData.name, isReprompt: true });
+            }
+        } else {
+            // Valid city received
+            // Return control to calling dialog
+            // Pass the city in the response property of results
+            session.endDialogWithResult({ response: age });
+        }
+    }
+];
