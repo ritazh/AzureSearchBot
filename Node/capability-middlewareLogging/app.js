255383a80e018f249866587e64dbea516967ac70
Christopher Harrison <chrhar@microsoft.com>
2017-01-24 13:48:16 -0800

    Added logging and middleware for Node

---
+++ Node/capability-middlewareLogging/app.js
@@ -0,0 +1,34 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+var myMiddleware = require('./middleware');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector);
+
+// Middleware for logging
+bot.use({
+    botbuilder: function (session, next) {
+        myMiddleware.logIncomingMessage(session, next);
+    },
+    send: function (event, next) {
+        myMiddleware.logOutgoingMessage(event, next);
+    }
+
+})
+
+bot.dialog('/', [
+    function (session) {
+        session.send("This is a simple bot. Type 'secret' for the answer to everything.");
+    }
+]);
