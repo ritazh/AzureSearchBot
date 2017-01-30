4289329ff2ce3e3deb21de9029d1e23758e171dc
Christopher Harrison <chrhar@microsoft.com>
2017-01-29 16:23:28 -0800

    Updated to 3.5 style

--- Node/capability-middlewareLogging/app.js
+++ Node/capability-middlewareLogging/app.js
@@ -1,33 +1,34 @@
-var builder = require('botbuilder');
-var restify = require('restify');
-var myMiddleware = require('./middleware');
+const builder = require('botbuilder');
+const restify = require('restify');
 
-var server = restify.createServer();
+const server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
 
-var connector = new builder.ChatConnector({
+const connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector);
+const bot = new builder.UniversalBot(connector, (session) => {
+    session.send("This is a simple bot.");
+});
+
+const logUserConversation = (event) => {
+    console.log('message: ' + event.text + ', user: ' + event.address.user.name);
+};
 
 // Middleware for logging
 bot.use({
     receive: function (event, next) {
-        myMiddleware.logIncomingMessage(event, next, bot);
+        logUserConversation(event);
+        next();
     },
     send: function (event, next) {
-        myMiddleware.logOutgoingMessage(event, next);
+        logUserConversation(event);
+        next();
     }
-})
-
-bot.dialog('/', [
-    function (session) {
-        session.send("This is a simple bot. Type 'secret' for the answer to everything.");
-    }
-]);
+});
