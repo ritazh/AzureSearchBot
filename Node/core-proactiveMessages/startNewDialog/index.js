ce4cb606d68adf1115db93881d9157c0c36fb7a8
Ezequiel Jadib <ezequiel.jadib@southworks.net>
2017-02-01 18:24:41 -0300

    Add comments and refactored code

--- Node/core-proactiveMessages/startNewDialog/index.js
+++ Node/core-proactiveMessages/startNewDialog/index.js
@@ -1,13 +1,14 @@
-"use strict";
+'use strict';
 
 var restify = require('restify');
 var builder = require('botbuilder');
 var server = restify.createServer();
 
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url); 
+  console.log('%s listening to %s', server.name, server.url);
 });
 
+// setup bot credentials
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -16,48 +17,46 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 bot = require("./botadapter").patch(bot);
 
-bot.dialog('/survey', [
-  function (session, args, next) {
-    if (session.message.text=="done"){
-      session.send("Great, back to the original conversation");
-      session.endDialog();
-    }else{
-          session.send('Hello, I\'m the survey dialog. I\'m interrupting your conversation to ask you a question. Type "done" to resume');
-    }
-  },
-  function (session, results) {
- 
+// handle the proactive initiated dialog
+bot.dialog('/survey', function (session, args, next) {
+  if (session.message.text === "done") {
+    session.send("Great, back to the original conversation");
+    session.endDialog();
+  } else {
+    session.send('Hello, I\'m the survey dialog. I\'m interrupting your conversation to ask you a question. Type "done" to resume');
   }
-]);
+});
 
-function startProactiveDialog(addr) {
+// initiate a dialog proactively 
+function startProactiveDialog(address) {
   // set resume:false to resume at the root dialog
   // else true to resume the previous dialog
-  bot.beginDialog(savedAddress, "*:/survey", {}, { resume: true });  
+  bot.beginDialog(address, "*:/survey", {}, {resume: true});
 }
 
 var savedAddress;
 server.post('/api/messages', connector.listen());
-server.get('/api/CustomWebApi', (req, res, next) => {
-    startProactiveDialog(savedAddress);
-    res.send('triggered');
-    next();
-  }
-);
 
-bot.dialog('/', function(session, args) {
+// Do GET this endpoint to start a dialog proactively
+server.get('/api/CustomWebApi', function (req, res, next) {
+  startProactiveDialog(savedAddress);
+  res.send('triggered');
+  next();
+});
+
+// root dialog
+bot.dialog('/', function (session, args) {
 
   savedAddress = session.message.address;
 
   var message = 'Hey there, I\'m going to interrupt our conversation and start a survey in a few seconds.';
   session.send(message);
-  
-  connector.url
+
   message = 'You can also make me send a message by accessing: ';
   message += 'http://localhost:' + server.address().port + '/api/CustomWebApi';
   session.send(message);
 
   setTimeout(() => {
     startProactiveDialog(savedAddress);
-  }, 5000)
+  }, 5000);
 });
